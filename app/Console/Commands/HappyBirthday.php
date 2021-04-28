5908c05801ba8f78c9ba261464e61f20260eaa18
Edon Sekiraqa <edon@frakton.com>
2021-04-28 19:50:46 +0200

    PHP artisan commands and user input

--- app/Console/Commands/HappyBirthday.php
+++ app/Console/Commands/HappyBirthday.php
@@ -42,10 +42,37 @@ class HappyBirthday extends Command
         $now = Carbon::parse($this->argument('birthdate'));
         $students = Student::where('birthdate', $now)->get();
 
-        if($students){
-            foreach ($students as $student){
-                $this->info("Send happy birthday message for student: ".$student->first_name);
-            }
+        $studentCount = count($students);
+
+        if ($studentCount == 0) {
+            $this->info("No students found!");
+            return;
+        }
+
+        $sendLimit = $this->ask($studentCount . " students found. Please type the number of students who will receive the email");
+        $sendLimit = intval($sendLimit);
+
+        if ($sendLimit > $studentCount || $sendLimit == 0) {
+            $this->error("The send limit is invalid!");
+            return;
+        }
+
+        $password = $this->secret("Type the password to proceed");
+
+        if ($password != "1234") {
+            $this->error("Not authenticated!");
+            return;
+        }
+
+        for ($i = 0; $i < $sendLimit; $i++) {
+            $student = $students[$i];
+            $this->info("Send happy birthday message for student: " . $student->first_name);
+        }
+
+        if($this->confirm("Do you want to exit?")){
+            $this->info("Exit!");
+        }else{
+            $this->info("Don't Exit!");
         }
 
         return 0;

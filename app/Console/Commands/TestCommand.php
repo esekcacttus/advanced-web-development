be2ddc44e5c9c3b68e1e45bf6a00d8ed3db827b6
Edon Sekiraqa <edon@frakton.com>
2021-04-28 20:01:24 +0200

    Artisan Command Auto Complete

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Student;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command tests';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->anticipate("Type your name by ARRAY", ['Filan', 'Filane', 'Fistek', 'John']);
+
+        $this->info("The name by ARRAY: ".$name);
+
+        $studentName = $this->anticipate("Type the name of student", function ($input){
+            $studentNames = Student::where('first_name', 'LIKE', '%'.$input.'%')
+                ->select('first_name')->get()->toArray();
+
+            $names = [];
+            foreach ($studentNames as $studentName){
+                $names[] = $studentName['first_name'];
+            }
+
+            return $names;
+        });
+
+        $this->info("The name by DATABASE: ".$studentName);
+        return 0;
+    }
+}
